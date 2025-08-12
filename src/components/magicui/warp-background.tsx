4050026402c85a9600ea28bbe6e7de4c58322159
Miguel-drm <mglderoma@gmail.com>
2025-08-12 22:16:10 +0800

    finalize

--- src/components/magicui/warp-background.tsx
+++ src/components/magicui/warp-background.tsx
@@ -84,7 +84,7 @@ export const WarpBackground: React.FC<WarpBackgroundProps> = ({
   const leftBeams = useMemo(() => generateBeams(), [generateBeams]);
 
   return (
-    <div className={cn("relative rounded border p-10 lg:p-20", className)} {...props}>
+    <div className={cn("relative rounded p-10 lg:p-20", className)} {...props}>
       <div
         style={
           {
