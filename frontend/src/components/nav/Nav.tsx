31e2975257e2ebcb7d43e8d159d29c547d3ea894
Miguel-drm <mglderoma@gmail.com>
2026-02-20 13:46:32 +0800

    .

--- frontend/src/components/nav/Nav.tsx
+++ frontend/src/components/nav/Nav.tsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
 import { WordRotate } from "../magicui/word-rotate";
 import { AnimatedThemeToggler } from "../magicui/animated-theme-toggler";
 import LiveListenersBadge from "@/components/LiveListenerBadge/LiveListenersBadge";
-import { ModalForm } from "../ui/modal-form";
+
 
 const Nav = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleWordClick = (word: string) => {
-    if (word === "Miguel") {
-      setIsModalOpen(true);
-    }
-  };
 
   return (
     <>
@@ -20,7 +13,6 @@ const Nav = () => {
           <WordRotate 
             words={["Miguel", "Music", "Playlist"]} 
             className="text-[clamp(1.5rem,6vw,3rem)] font-bold md:text-3xl lg:text-5xl cursor-pointer font-spotify-display tracking-tight" 
-            onWordClick={handleWordClick}
           />
         </div>
         <div className="flex items-center gap-8">
